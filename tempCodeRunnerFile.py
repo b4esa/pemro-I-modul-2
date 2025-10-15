4bd49c62b1c6fd469583027180718103939fe1eb
b4esa <tysaluthfia1@gmail.com>
2025-10-15 10:14:35 +0700

    tysa cantik

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,4 +1,8 @@
-a= float (input ("Masukkan Nilai Pertama :"))
-b= float (input ("Masukkan Nilai Kedua :"))
-hasil=a+b
-print (f"Hasil dari penjumlahan nilai pertama \"{a}\" dan nilai Kedua \"{b}\" adalah \"{hasil:.2f}\"")
+a = float(input("Masukkan Nilai Pertama : "))
+b = float(input("Masukkan Nilai Kedua : "))
+hasil = a + b
+
+if a.is_integer():
+    print(f'Hasil dari penjumlahan nilai pertama "{int(a)}" dan nilai kedua "{b:.2f}" adalah "{hasil:.2f}"')
+else:
+    print(f'Hasil dari penjumlahan nilai pertama "{a:.2f}" dan nilai kedua "{b:.2f}" adalah "{hasil:.2f}"')
