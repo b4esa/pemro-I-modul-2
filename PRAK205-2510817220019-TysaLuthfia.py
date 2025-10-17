db2fb435ea64024ac43587713ac6c8bd2ee3fccd
b4esa <tysaluthfia1@gmail.com>
2025-10-17 21:47:24 +0700

    tysa cantik

--- PRAK205-2510817220019-TysaLuthfia.py
+++ PRAK205-2510817220019-TysaLuthfia.py
@@ -1,24 +1,23 @@
 import math
 
-angka = []
+A = int (input())
+B = int (input())
 
-inputan = input().split()
+alas = math.sqrt(B * B - A * A)
+kel = math.sqrt (A + B + alas)
+lu = math.sqrt (A * alas / 2)
 
-angka.extend(inputan)
+print(f"Alas         = {alas:.0f} cm")
+print(f"Tinggi       = {A:.0f} cm")
+print(f"Keliling     = {kel:.0f} cm")
+print(f"Luas         = {lu:.0f} cm^2")
+A, B = map(int, input().split())
 
-while(len(angka) < 2):
-    inputan = input().split()
-    angka.extend(inputan)
+alas = math.sqrt(B * B - A * A)
+kel = math.sqrt (A + B + alas)
+lu = math.sqrt (A * alas / 2)
 
-a = float(angka[0])
-b = float(angka[1])
-
-c = math.sqrt((b * b) - (a * a))
-keliling = a + b + c
-luas = 0.5 * c * a
-
-print(" ")
-print(f"Alas = {c:.0f} cm")
-print(f"Tinggi = {a:.0f} cm")
-print(f"Keliling = {keliling:.0f} cm")
-print(f"Luas = {luas:.0f} cm^2\n")
+print(f"Alas         = {alas:.0f} cm")
+print(f"Tinggi       = {A:.0f} cm")
+print(f"Keliling     = {kel:.0f} cm")
+print(f"Luas         = {lu:.0f} cm^2")
